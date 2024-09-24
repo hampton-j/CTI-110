70f0ad38a2009409a427d55dd4a0c2125236c8eb
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-09-24 14:34:38 -0400

    Add files via upload

---
+++ p1lab1.py
@@ -0,0 +1,29 @@
+# CTI 110
+# P1LAB - My First Program
+# HamptonJ
+# 9/24/24
+
+print("Hello World")
+print("This is a Python program") 
+
+
+
+
+print("Hello!")
+#greet the user by name
+#TODO: ask their name
+print("What's your name?")
+print("Enter first name:")
+first = input()
+print("Enter lastname:")
+last = input()
+
+#say their name
+print("Nice to meet you , ",first, last)
+
+#ask them if they like stuff
+print("What do you like?")
+topic = input()
+print("Cool! I like" , topic, "too.")
+
+      
