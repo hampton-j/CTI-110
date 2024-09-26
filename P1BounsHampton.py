86425a3fb60469a983a1ebd9f990897281fb809a
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-09-26 14:24:34 -0400

    Add files via upload

---
+++ P1BounsHampton.py
@@ -0,0 +1,52 @@
+#CTI 110
+#M1BOUNS - Turtles
+# Jaiden
+# 9/26/24
+
+import turtle
+
+t = turtle.Turtle()
+
+t.pensize(3)
+t.fillcolor("orchid")
+t.begin_fill() 
+t.forward(100)
+t.left(90)
+t.forward(90)
+t.left(90)
+t.left(90)
+t.forward(90)
+t.right(100)
+t.forward(90)
+t.left(90)
+t.right(100)
+t.forward(90)
+t.rt(100)
+t.lt(90)
+t.fd(80)
+t.fd(360)
+t.rt(100)
+t.fd(90)
+t.rt(90)
+t.fd(100)
+t.fd(100)
+t.fd(100)
+t.fd(100)
+t.fd(90)
+t.lt(80)
+t.lt(80)
+t.lt(80)
+t.fd(90)
+t.rt(90)
+t.circle(50)
+t.circle(80,180)
+t.circle(80,180)
+t.circle(80,180)
+t.circle(100,180)
+
+
+t.end_fill()
+
+
+
+
