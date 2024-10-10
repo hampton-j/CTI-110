40b4d61d292c166c5d42edf58e5ea5cb85ffa99f
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-10-10 14:37:26 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+# test
+print("What's your name?")
+name = input()
+print("Hi" , name)
+# or this way
+name = input("What's your name?")
+print("Hi", name)
+
+# numbers
+word = input("What's the secret word?")
+number = int(input("Type in a number: "))
+price = float(input("The cost is: $"))             
