7eb45fc99d2236edeb940a87b244c7d8c72f6ed5
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-10-10 14:32:03 -0400

    Add files via upload

---
+++ p2lab1_Hampton.py
@@ -0,0 +1,21 @@
+# CTI 110
+#P2LAB1
+#Hampton
+# 10/10/24
+
+# This program adds up sales of something.
+
+
+print("What's your name")
+name = input()
+# numbers
+
+scent = input (" What type of scent? ")
+number_of_candles = int(input("How may candles will you buy? "))
+price = float(input("How much will each one cost: $"))
+
+overall_cost = number_of_candles * price
+print(overall_cost)
+print("Your overall cost is", number_of_candles * price)
+
+
