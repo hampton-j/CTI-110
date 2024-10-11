06bd0ab360d1f91fe6272b850bf8e051b2e3a13f
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-10-11 12:25:46 -0400

    Add files via upload

---
+++ P1HW1_Hampton.py
@@ -0,0 +1,25 @@
+# CTI-110
+# P1HW1
+# Hampton
+# 10/11/24
+
+
+#This program will be using integers
+
+
+base = int(input("what is the base: "))
+exponent = int(input(" what is the exponent: "))
+answer = base ** exponent
+print(base," to the power of",exponent, "is", answer)
+
+
+
+first = int(input(" Enter a starting integer: "))
+second = int(input(" Enter an integer to add: "))
+third = int(input(" Enter an integer to subtract: "))
+sum = first + second - third
+print(first,"+",second,"-",third,"is equal to",sum)
+
+
+
+
