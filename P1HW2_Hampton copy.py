0b57a8c05807b1a9dce7afb0fae9110cfc9c4f2e
hampton-j <hamptonj2880@student.faytechcc.edu>
2024-10-11 12:40:22 -0400

    Add files via upload

--- P1HW2_Hampton copy.py
+++ P1HW2_Hampton copy.py
@@ -18,16 +18,17 @@ print (" Trip Calculator ")
 # ask user to enter their budget
 budget = float(input("What is your travel budget? $ "))
 # Where are they going
-dest = input("Where are you are going?")
-gas = float(input ("how much is spent on gas?"))
-hotel = float(input("how much is spent on hotel?"))
-food = float(input ("how much is spent on food?"))
+dest = input("Where are you are going? ")
+gas = float(input ("how much is spent on gas? "))
+hotel = float(input("how much is spent on hotel? "))
+food = float(input ("how much is spent on food? "))
 expenses = gas + food + hotel
-print("total",expenses)
-----------TravelExpenses--------
-print("location:", Italy)
-print("Initial Budget:", 700)
+remaining = budget - expenses
+print("----------TravelExpenses--------")
+print("location:", dest)
+print("Initial Budget:", budget)
 print("fuel:", gas)
 print ("accomodation:",hotel)
 print("food:",food)
-print("remaining balance:",200)
+print("total",expenses)
+print("remaining balance:",remaining)
